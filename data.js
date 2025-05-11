3eddc2ce937ce5fb24d5dc5d016f0e316bbd8a93
becm23 <becm23series@gmail.com>
2025-05-11 16:40:14 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+  { name: "Sheuly Akter", role: "President" },
+  { name: "Shelina Sultana", role: "Member" },
+  { name: "Hasan Mahmud", role: "Secretary" },
+  { name: "Rahim Uddin", role: "Treasurer" },
+];
